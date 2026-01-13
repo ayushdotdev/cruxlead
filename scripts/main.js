1dde45a631470c73f8d324a7f2b04b23d791906b
ayushdotdev <ayush438500@gmail.com>
2026-01-14 05:21:50 +0530

    SCripting

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,33 @@ const leadEmail = document.getElementById("email")
 const saveLead = document.getElementById("save-lead")
 const exportExcel = document.getElementById("export")
 const clearLeads = document.getElementById("clear")
+const leadList = document.getElementById("lead-list")
 
-const storedLeads = JSON.parse( localstorage.getItem("Leads") )
+const storedLeads = JSON.parse( localStorage.getItem("Leads") )
 
 if (storedLeads) {
   leads = storedLeads
+  render(leads)
 }
 
-funtion render(leads) {
+function render(leads) {
   let items = ""
 
   for (let i = 0; i < leads.length; i++) {
     items += `
-      div
-
-
+      <p> ${leads[i][0]} (<span> ${leads[i][1]} </span>) </p>
     `
-
+    
   }
+  leadList.innerHTML = items
 }
+
+
+saveLead.addEventListener("click", function(){
+  leads.push([leadName.value, leadEmail.value])
+  leadName.value = ''
+  leadEmail.value = ''
+  localStorage.setItem("leads", JSON.stringify(leads))
+  render(leads)
+
+})
